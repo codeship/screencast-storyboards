aeb9dd05469ac038083cdecf5feef3c3f82b3fd7
Florian Motlik <flomotlik@gmail.com>
2014-04-29 15:59:29 +0200

    Use the github raw url

--- lib/tags/image_path.rb
+++ lib/tags/image_path.rb
@@ -1,7 +1,7 @@
 class ImagePath < Liquid::Tag
   def render(context)
     assigns = context.scopes[1]
-    "https://github.com/codeship/screencast-storyboards/tree/#{assigns['technology_slug']}-#{assigns['repository_slug']}-#{assigns['deployment_slug']}"
+    "https://raw.githubusercontent.com/codeship/screencast-storyboards/#{assigns['technology_slug']}-#{assigns['repository_slug']}-#{assigns['deployment_slug']}"
   end
 end
 
